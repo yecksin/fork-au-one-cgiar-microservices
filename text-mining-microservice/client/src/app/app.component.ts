f89e021d8c4a16e1df1c87e815e0e9a5a545e648
xKeCo <kevcollazos@gmail.com>
2024-06-04 16:27:04 -0500

    :sparkles: feat(Upload File): added upload file component

--- text-mining-microservice/client/src/app/app.component.ts
+++ text-mining-microservice/client/src/app/app.component.ts
@@ -1,13 +1,83 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MessageService, PrimeNGConfig } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { FileUploadModule } from 'primeng/fileupload';
+import { BadgeModule } from 'primeng/badge';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { ToastModule } from 'primeng/toast';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+interface Evidence {
+  file?: File | null;
+  sp_file_name: string | null;
+  percentage?: string | number;
+  link?: string | null;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FileUploadModule, ButtonModule, BadgeModule, ProgressBarModule, ToastModule, HttpClientModule, CommonModule],
+  providers: [MessageService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'client';
+
+  files = [];
+
+  totalSize: number = 0;
+
+  totalSizePercent: number = 0;
+
+  constructor(private config: PrimeNGConfig, private messageService: MessageService) {}
+
+  choose(event: any, callback?: any) {
+    callback();
+  }
+
+  onRemoveTemplatingFile(event: any, file: any, removeFileCallback: any, index: any) {
+    removeFileCallback(event, index);
+    this.totalSize -= parseInt(this.formatSize(file.size));
+    this.totalSizePercent = this.totalSize / 10;
+  }
+
+  onClearTemplatingUpload(clear: any) {
+    clear();
+    this.totalSize = 0;
+    this.totalSizePercent = 0;
+  }
+
+  onTemplatedUpload() {
+    this.messageService.add({ severity: 'info', summary: 'Success', detail: 'File Uploaded', life: 3000 });
+  }
+
+  onSelectedFiles(event: any) {
+    this.files = event.currentFiles;
+    this.files.forEach((file: any) => {
+      this.totalSize += parseInt(this.formatSize(file.size));
+    });
+    this.totalSizePercent = this.totalSize / 10;
+  }
+
+  uploadEvent(callback: any) {
+    callback();
+  }
+
+  formatSize(bytes: any) {
+    const k = 1024;
+    const dm = 3;
+    const sizes = this.config.translation.fileSizeTypes as any;
+    if (bytes === 0) {
+      return `0 ${sizes[0]}`;
+    }
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    const formattedSize = parseFloat((bytes / Math.pow(k, i)).toFixed(dm));
+
+    return `${formattedSize} ${sizes[i]}`;
+  }
 }
