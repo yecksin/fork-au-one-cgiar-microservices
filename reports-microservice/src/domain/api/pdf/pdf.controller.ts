b5a8b86a398acc84da8c81564c2e510e60c57d64
Juan Delgado <juandelgadog98@gmail.com>
2024-07-18 16:44:41 -0500

    ♻️ refactor(Reports microservice): Added pdf-creator-node lib and services

--- reports-microservice/src/domain/api/pdf/pdf.controller.ts
+++ reports-microservice/src/domain/api/pdf/pdf.controller.ts
@@ -13,34 +13,12 @@ export class PdfController {
     private readonly pdfService: PdfService,
   ) {}
 
-  @Post('generate-puppeteer')
-  async generatePdfHttpNodePuppeteer(
-    @Body() createPdfDto: CreatePdfDto,
-    @Res() res: Response,
-  ): Promise<void> {
-    const pdf = await this.pdfService.generatePdfPuppeteer(createPdfDto);
-    res.set({
-      'Content-Type': 'application/pdf',
-      'Content-Disposition': 'attachment; filename=report.pdf',
-      'Content-Length': pdf.length,
-    });
-    res.end(pdf);
-  }
-
-  @MessagePattern('generate-puppeteer')
-  async generatePdfNodePuppeteer(
-    @Payload() createPdfDto: CreatePdfDto,
-  ): Promise<Buffer> {
-    const pdf = await this.pdfService.generatePdfPuppeteer(createPdfDto);
-    return pdf;
-  }
-
-  // @Post('generate')
-  // async generatePdfHttpNode(
+  // @Post('generate-puppeteer')
+  // async generatePdfHttpNodePuppeteer(
   //   @Body() createPdfDto: CreatePdfDto,
   //   @Res() res: Response,
   // ): Promise<void> {
-  //   const pdf = await this.pdfService.generatePdf(createPdfDto);
+  //   const pdf = await this.pdfService.generatePdfPuppeteer(createPdfDto);
   //   res.set({
   //     'Content-Type': 'application/pdf',
   //     'Content-Disposition': 'attachment; filename=report.pdf',
@@ -49,13 +27,35 @@ export class PdfController {
   //   res.end(pdf);
   // }
 
-  // @MessagePattern('generate')
-  // async generatePdfNode(
+  // @MessagePattern('generate-puppeteer')
+  // async generatePdfNodePuppeteer(
   //   @Payload() createPdfDto: CreatePdfDto,
   // ): Promise<Buffer> {
-  //   const pdf = await this.pdfService.generatePdf(createPdfDto);
+  //   const pdf = await this.pdfService.generatePdfPuppeteer(createPdfDto);
   //   return pdf;
   // }
+
+  @Post('generate')
+  async generatePdfHttpNode(
+    @Body() createPdfDto: CreatePdfDto,
+    @Res() res: Response,
+  ): Promise<void> {
+    const pdf = await this.pdfService.generatePdf(createPdfDto);
+    res.set({
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': 'attachment; filename=report.pdf',
+      'Content-Length': pdf.length,
+    });
+    res.end(pdf);
+  }
+
+  @MessagePattern('generate')
+  async generatePdfNode(
+    @Payload() createPdfDto: CreatePdfDto,
+  ): Promise<Buffer> {
+    const pdf = await this.pdfService.generatePdf(createPdfDto);
+    return pdf;
+  }
 
   @Post('subscribe-application')
   async subscribeApplication(@Body() newApplication: SubscribeApplicationDto) {
--- reports-microservice/src/domain/api/pdf/pdf.service.ts
+++ reports-microservice/src/domain/api/pdf/pdf.service.ts
@@ -3,64 +3,66 @@ import { CreatePdfDto } from './dto/create-pdf.dto';
 import { SubscribeApplicationDto } from './dto/subscribe-application.dto';
 import { ClarisaService } from '../../tools/clarisa/clarisa.service';
 import { ResponseUtils } from '../../utils/response.utils';
-import * as handlebars from 'handlebars';
-import * as puppeteer from 'puppeteer';
+import { create as createPDF } from 'pdf-creator-node';
+// import * as handlebars from 'handlebars';
+// import * as puppeteer from 'puppeteer';
+import { ReadStream } from 'fs';
 
 @Injectable()
 export class PdfService {
   private readonly _logger = new Logger(PdfService.name);
   constructor(private readonly _clarisaService: ClarisaService) {}
 
-  async generatePdfPuppeteer(createPdfDto: CreatePdfDto): Promise<Buffer> {
-    console.log('🚀 ~ PdfService ~ generatePdf ~ createPdfDto:', createPdfDto);
-    const { data, templateData, options } = createPdfDto;
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+  // async generatePdfPuppeteer(createPdfDto: CreatePdfDto): Promise<Buffer> {
+  //   console.log('🚀 ~ PdfService ~ generatePdf ~ createPdfDto:', createPdfDto);
+  //   const { data, templateData, options } = createPdfDto;
+  //   const browser = await puppeteer.launch();
+  //   const page = await browser.newPage();
 
-    const template = handlebars.compile(templateData);
-    const html = template(data);
+  //   const template = handlebars.compile(templateData);
+  //   const html = template(data);
 
-    await page.setContent(html);
-    const pdf = await page.pdf(options);
-    console.log('🚀 ~ PdfService ~ generatePdf ~ pdf:', pdf);
+  //   await page.setContent(html);
+  //   const pdf = await page.pdf(options);
+  //   console.log('🚀 ~ PdfService ~ generatePdf ~ pdf:', pdf);
 
-    await browser.close();
-    if (!pdf) throw new Error('Error generating pdf');
+  //   await browser.close();
+  //   if (!pdf) throw new Error('Error generating pdf');
 
-    console.info('Pdf generated successfully');
-    return pdf;
+  //   console.info('Pdf generated successfully');
+  //   return pdf;
+  // }
+
+  async generatePdf(createPdfDto: CreatePdfDto): Promise<Buffer> {
+    try {
+      const { data, templateData, options } = createPdfDto;
+      const document = {
+        html: templateData,
+        data: data,
+        type: 'stream',
+      };
+
+      const pdfStream: ReadStream = await createPDF(document, options);
+
+      if (!pdfStream) throw new Error('Error converting pdf to stream');
+
+      const pdfBuffer: Buffer = await this.streamToBuffer(pdfStream);
+      console.info('PDF generated successfully');
+      return pdfBuffer;
+    } catch (error) {
+      this._logger.error(`Error generating pdf: ${error}`);
+      throw new Error(`Error generating pdf ${error}`);
+    }
   }
 
-  // async generatePdf(createPdfDto: CreatePdfDto): Promise<Buffer> {
-  //   try {
-  //     const { data, templateData, options } = createPdfDto;
-  //     const document = {
-  //       html: templateData,
-  //       data: data,
-  //       type: 'stream',
-  //     };
-
-  //     const pdfStream: ReadStream = await createPDF(document, options);
-
-  //     if (!pdfStream) throw new Error('Error converting pdf to stream');
-
-  //     const pdfBuffer: Buffer = await this.streamToBuffer(pdfStream);
-  //     console.info('PDF generated successfully');
-  //     return pdfBuffer;
-  //   } catch (error) {
-  //     this._logger.error(`Error generating pdf: ${error}`);
-  //     throw new Error(`Error generating pdf ${error}`);
-  //   }
-  // }
-
-  // public streamToBuffer(stream: ReadStream): Promise<Buffer> {
-  //   return new Promise((resolve, reject) => {
-  //     const chunks: Buffer[] = [];
-  //     stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
-  //     stream.on('end', () => resolve(Buffer.concat(chunks)));
-  //     stream.on('error', (err) => reject(err));
-  //   });
-  // }
+  public streamToBuffer(stream: ReadStream): Promise<Buffer> {
+    return new Promise((resolve, reject) => {
+      const chunks: Buffer[] = [];
+      stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
+      stream.on('end', () => resolve(Buffer.concat(chunks)));
+      stream.on('error', (err) => reject(err));
+    });
+  }
 
   async subscribeApplication(newApplication: SubscribeApplicationDto) {
     try {
