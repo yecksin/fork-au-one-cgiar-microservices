2aceb05ce094db44c7926e6b4b52b2b51c1db1e0
davinzifc <47566127+davinzifc@users.noreply.github.com>
2024-08-29 15:19:03 -0500

    :recycle: refactor(coverage) updated package.json coverage thresholds

--- notification-microservice/server/src/domain/api/mailer/dto/ubscribe-application.dto.ts
+++ notification-microservice/server/src/domain/api/mailer/dto/ubscribe-application.dto.ts
@@ -1,4 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class SubscribeApplicationDto {
+  @ApiProperty({
+    required: true,
+    description: 'Acronym of the application',
+    example: 'APP',
+    name: 'acronym',
+    type: String,
+  })
   public acronym: string;
+  @ApiProperty({
+    required: true,
+    description: 'Name of the application',
+    example: 'Application',
+    name: 'environment',
+    type: String,
+  })
   public environment: string;
 }
