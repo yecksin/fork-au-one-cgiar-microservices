76f43d64bff18191b686d795e66412f1939d406c
davinzifc <47566127+davinzifc@users.noreply.github.com>
2024-08-30 11:15:08 -0500

    :recycle: refactor(Base route): The base route is configured

--- notification-microservice/server/src/app.controller.ts
+++ notification-microservice/server/src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ResponseUtils } from './domain/shared/utils/response.utils';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  mainPage() {
-    return 'hola mundo';
+  mainPage(@Req() req: Request) {
+    return ResponseUtils.format({
+      description: 'Notification Microservice',
+      status: HttpStatus.OK,
+      data: {
+        message: 'Welcome to the Notification Microservice',
+        author: 'One CGIAR - IBD',
+        ip: req.ip,
+        client: req.headers['user-agent'],
+      },
+    });
   }
 }
