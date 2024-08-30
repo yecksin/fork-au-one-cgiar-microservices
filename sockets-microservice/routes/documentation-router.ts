f55517e45f2f068e5a5a494139584f5777ccb928
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-08-30 11:33:13 -0500

    :sparkles: feat: Angular doc added

--- sockets-microservice/routes/documentation-router.ts
+++ sockets-microservice/routes/documentation-router.ts
@@ -6,11 +6,3 @@ export const documentationRouter = Router();
 documentationRouter.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
-
-documentationRouter.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', 'about.html'));
-});
-
-documentationRouter.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', 'contact.html'));
-});
